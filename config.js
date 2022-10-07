7a8856331686387b197391130fc438038a0dc5b0
aidilm01 <110966734+aidilm01@users.noreply.github.com>
2022-10-07 19:39:44 +0700

    Update config.js

--- config.js
+++ config.js
@@ -44,11 +44,11 @@ global.Intervalmsg = 1800 //detik
 
 //========Url Template Buttons=========//
 global.dtu = 'ɪɴꜱᴛᴀɢʀᴀᴍ'
-global.urlnya = "https://www.instagram.com/aguzfamilia"
+global.urlnya = "https://www.instagram.com/dilzpx_"
 
 //============= callButtons =============//
 global.dtc = 'ᴄᴀʟʟ ᴏᴡɴᴇʀ'
-global.phn = '+62 813-2017-0984'
+global.phn = '+62 813-8044-8281'
 
 //============= Games ================//
 global.benar = '_*Benar✅*_'
@@ -63,7 +63,7 @@ global.author = data.owner;
 
 //=========== Requirements ==========//  
         global.colong1 = 'Ciss 📸'
-        global.colong2 = 'ʙy ᴀɢᴜᴢ ꜰᴀᴍɪʟɪᴀ'
+        global.colong2 = 'ʙy DILZPX ꜰᴀᴍɪʟɪᴀ'
 global.img = pickRandom(waifu)
 global.fla = pickRandom(flaaa)
 
